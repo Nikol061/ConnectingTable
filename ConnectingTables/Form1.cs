c72af7f3e23fa01719b094c52a3fd7aef5ff3c92
nikolkoleva48 <nikolkoleva48@gmail.com>
2024-04-16 11:37:42 +0300

    Connection String

---
+++ ConnectingTables/Form1.cs
@@ -0,0 +1,23 @@
+using MySqlConnector;
+
+namespace ConnectingTables
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string connstr = "server = 10.6.0.127;" +
+            " port = 3306; " +
+            "user = PC1; " +
+            "password = 1111;" +
+            " database = trees_zaimov";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MySqlConnection connect = new MySqlConnection(connstr);
+            if(connect.State == 0) connect.Open();
+            MessageBox.Show("Connection NOW opened");
+        }
+    }
+}
